bfbc33745cbfc0f3c72751428287ba5ac163af7c
Andrii Kurinnyi <andrew@marpasoft.com>
2011-04-02 10:43:33 +0000

    Refactored profile retrieval process into separate function, if user is already authenticated and profile doesn't have user attached (e.g. just created), attach profile to existing user

--- linked_accounts/backends.py
+++ linked_accounts/backends.py
@@ -1,17 +1,6 @@
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-from django.utils.importlib import import_module
-
 from django.contrib.auth.models import User
 
-from linked_accounts import LINKED_ACCOUNTS_HANDLERS
-
-
-HANDLERS = getattr(
-    settings,
-    'LINKED_ACCOUNTS_HANDLERS',
-    LINKED_ACCOUNTS_HANDLERS
-)
+from linked_accounts.utils import get_profile
 
 
 class LinkedAccountsBackend(object):
@@ -19,13 +8,5 @@ class LinkedAccountsBackend(object):
     def get_user(self, user_id):
         return User.objects.get(id=user_id)
 
-    def authenticate(service=None, token=None):
-        handler_module = dict(HANDLERS).get(service, None)
-        if handler_module:
-            module, handler = handler_module.rsplit('.', 1)
-            handler_class = getattr(import_module(module), handler)
-            handler = handler_class()
-            profile = handler.get_profile(token)
-            return profile.user
-        else:
-            raise ImproperlyConfigured('No handler for service %s' % service)
+    def authenticate(self, service=None, token=None):
+        profile = get_profile(service, token)
--- linked_accounts/views.py
+++ linked_accounts/views.py
@@ -3,13 +3,18 @@ from django.http import HttpResponseRedirect
 
 import django.contrib.auth as auth
 
+from linked_accounts.utils import get_profile
+
 
 class AuthCallback(object):
     def __call__(self, request, access, token):
         next = request.POST.get('next', settings.LOGIN_REDIRECT_URL)
         service = access.service
         if request.user.is_authenticated():
-            pass
+            profile = get_profile(service=service, token=token)
+            if not profile.user:
+                profile.user = request.user
+                profile.save()
         else:
             user = auth.authenticate(service=service, token=token)
             if user:
