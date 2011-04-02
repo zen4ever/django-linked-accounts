05bd64bf875dd915f96cce549fe2c8704f283c54
Andrii Kurinnyi <andrew@marpasoft.com>
2011-04-02 02:02:52 -0700

    Added convenience property to convert api_response to python object

--- linked_accounts/models.py
+++ linked_accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import simplejson as json
 
 
 class LinkedAccount(models.Model):
@@ -6,6 +7,10 @@ class LinkedAccount(models.Model):
     service = models.CharField(max_length=255, db_index=True)
     user = models.ForeignKey('auth.User', null=True)
     api_response = models.TextField(default='', blank=True)
+
+    @property
+    def api_response_data(self):
+        return json.loads(self.api_response)
 
     class Meta:
         unique_together = ('identifier', 'service')
