8e589d8f609b4136cae3f8bf27f3d77200a2ffba
Bryan Landers <bryanlanders@gmail.com>
2011-05-14 17:09:46 -0700

    Moved login and registration views out of linked-accounts app. Should be handled in accounts app in project.

--- linked_accounts/views.py
+++ linked_accounts/views.py
@@ -1,13 +1,9 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.views.generic.simple import direct_to_template
-
 import django.contrib.auth as auth
 
-from linked_accounts.models import LinkedAccount
 from linked_accounts.handlers import AuthHandler
-from linked_accounts.forms import RegisterForm
 
 
 LINKED_ACCOUNTS_ID_SESSION = getattr(
@@ -54,42 +50,3 @@ class AuthCallback(object):
 def oauth_access_success(request, access, token):
     callback = AuthCallback()
     return callback(request, access, token)
-
-def login(request, template_name="linked_accounts/login.html"):
-    next_url = request.REQUEST.get('next', settings.LOGIN_REDIRECT_URL)
-    request.session[LINKED_ACCOUNTS_NEXT_KEY] = next_url
-    return direct_to_template(request, template_name)
-
-def register(request, template_name="linked_accounts/registration.html"):
-    next_url = request.REQUEST.get('next', settings.LOGIN_REDIRECT_URL)
-
-    try:
-        profile_id = request.session[LINKED_ACCOUNTS_ID_SESSION]
-        profile = LinkedAccount.objects.get(id=profile_id)
-    except (KeyError, LinkedAccount.DoesNotExist):
-        return HttpResponseRedirect(next_url)
-
-    initial_data = {
-        'username': profile.username
-    }
-
-    email = profile.email
-    if email:
-        initial_data['email'] = email
-
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-
-        if form.is_valid():
-            user = form.save(profile)
-            user.backend = "linked_accounts.backends.LinkedAccountsBackend"
-            auth.login(request, user)
-            return HttpResponseRedirect(next_url)
-    else:
-        form = RegisterForm(initial=initial_data)
-
-    return direct_to_template(
-        request,
-        template_name,
-        {'form': form, 'profile': profile, 'next': next_url}
-    )
